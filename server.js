172c207cd380bb52a2f6e8580f5b247475a5debe
Sushmeet Sunger <ssunger@shutterstock.com>
2019-09-23 16:41:08 -0400

    close the server on detection of SIGINT

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const app = require("./app");
+const app = require('./app');
 const PORT = 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+process.on('SIGINT', function() {
+  console.log(
+    'SIGINT Interrupt signal detected and exiting node process now.'
+  );
+  server.close();
+});
+
 module.exports = app;
+``
