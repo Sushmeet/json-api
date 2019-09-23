4f7f5f8883524a5fd11f083cf2051982efc3f438
Sushmeet Sunger <ssunger@shutterstock.com>
2019-09-23 17:04:24 -0400

    add an end to end test for create product

--- src/test-acceptance/createProduct.spec.js
+++ src/test-acceptance/createProduct.spec.js
@@ -1,12 +1,57 @@
-const  app  = require("../../server");
+const app = require("../../server");
 const request = require("supertest");
+const db = require("knex")(require("../db/knexfile"));
 
 describe("Create Product", () => {
-  it("should create a product based on args supplied", async () => {
+  beforeAll(() => {
+    return db("products").del();
+  });
+  
+  afterAll(() => {
+    return db("products").del();
+  });
+  it("should ensure application is up and running", async () => {
     const result = await request(app)
-      .get('/')
-      .expect(200)
+      .get("/")
+      .expect(200);
 
-    expect(result.body.description).toEqual('Product API is running');
+    expect(result.body.description).toEqual("Product API is running");
+  });
+
+  it("should create a new product", async () => {
+    const product = {
+      name: "Red Shirt",
+      description: "Red hugo boss shirt",
+      category: "apparel",
+      brand: "Hugo Boss",
+      "tags": [
+        "red",
+        "shirt",
+        "slim fit"
+      ],
+    };
+    const expectedResult = [
+      {
+        "name": "Red Shirt",
+        "description": "Red hugo boss shirt",
+        "category": "apparel",
+        "brand": "Hugo Boss",
+        "tags": [
+          "red",
+          "shirt",
+          "slim fit"
+        ],
+      }
+    ];
+
+    const result = await request(app)
+      .post("/v1/products")
+      .send(product)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+      expect(result.body).toMatchObject(expectedResult);
+  
   });
 });
